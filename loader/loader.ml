3fb1f4e786f16bc1067326da60e5f2887326c05f
Einar Lielmanis <einars@gmail.com>
2012-07-15 16:26:54 +0300

    Ūdeņi salaboti (13866)

--- loader/loader.ml
+++ loader/loader.ml
@@ -19,6 +19,14 @@ type coords = int * int
 
 module CoordMap = Map.Make(struct type t = coords let compare = compare end)
 
+let rstrip ?(chars=" \t\r\n") s =
+  let p = 0
+  and l = ref ( (String.length s) - 1) in
+  while !l >= p && String.contains chars (String.unsafe_get s !l) do
+    decr l;
+  done;
+  String.sub s p (!l - p + 1)
+
 
 let (^+) c1 c2 =
   let x1, y1 = c1 and x2, y2 = c2
@@ -39,8 +47,7 @@ let make_2d_array n m init =
 
 (* }}} *)
 
-type elem = Wall | Rock | Lift | Earth | Empty | Lambda | Beard of int | Razor |
-    SysRobot | SysCandidates of (coords * elem) list
+type elem = Wall | Rock | Lift | Earth | Empty | Lambda | Beard of int | Razor | SysRobot
 
 let initial_robot_coordinates = ref (0,0)
 
@@ -99,7 +106,6 @@ let char_of_elem ?(lambdas = 1) = function
   | Beard _ -> "W"
   | Razor _ -> "!"
   | SysRobot -> failwith "char_of_elem certainly didn't expect any Robot"
-  | SysCandidates _ -> failwith "char_of_elem certainly didn't expect any Candidates"
 
 
 let load_line pos s accum =
@@ -121,7 +127,6 @@ let load_line pos s accum =
       | Beard _ -> CoordMap.add ps (Beard 0) rest
       | Empty -> rest
       | SysRobot -> failwith "I certainly didn't expect Robot in response to load_char"
-      | SysCandidates _ -> failwith "I certainly didn't expect Candidates in response to load_char"
     )
   in
   loop pos accum
@@ -155,7 +160,7 @@ let split_lines_to_field_and_metadata lines =
 
 let load_field lines =
   let rec loop pos = function
-    | line::rest -> load_line pos (String.strip line) ( loop (pos ^+ (0,1) ) rest )
+    | line::rest -> load_line pos (rstrip line) ( loop (pos ^+ (0,1) ) rest )
     | _ -> CoordMap.empty
   in
   let l_field, l_meta = split_lines_to_field_and_metadata lines in
@@ -186,11 +191,12 @@ let load_field lines =
   }
 
 
-let print_field f =
+let print_field ?(clear=true) f =
   let w, h = f.dimensions in
   let repr = make_2d_array (h + 1) (w + 1) " " in
 
-  printf "\x1b[2J\x1b[;H";
+  if clear then printf "\x1b[2J\x1b[;H";
+
   log "";
 
   let rx, ry = f.robot in repr.(ry).(rx) <- "R";
@@ -199,11 +205,11 @@ let print_field f =
   CoordMap.iter (fun (x,y) elem -> repr.(y).(x) <- char_of_elem ~lambdas:f.lambdas elem) f.f;
 
   for j = 1 to h do
-    printf "%s" (if (h - j + 1 >= f.water) then " " else "~");
+    printf "%s" (if (h - j >= f.water) then " " else "~");
     for k = 1 to w do
       printf "%s" repr.(h - j + 1).(k);
     done;
-    printf "%s" (if (h - j + 1 >= f.water) then " " else "~");
+    printf "%s" (if (h - j >= f.water) then " " else "~");
     printf "\n%!";
   done;
 
@@ -225,7 +231,8 @@ let string_of_elem_at f l = peek_map f.f l $ char_of_elem
 
 let rec is_empty field c = peek_map field.f c = Empty
 
-let rec is_walkable field direction c =
+let rec is_walkable field c =
+  (* does not check the rocks *)
   match peek_map field.f c with
   | Wall  -> false
   | Beard _ -> false
@@ -234,14 +241,8 @@ let rec is_walkable field direction c =
   | Empty -> true
   | Earth -> true
   | Lift  -> field.lambdas = 0
-  | Rock  ->
-      (* rocks can be pushed? *)
-      if direction = "L" then
-        is_empty field (c ^- (1, 0))
-      else if direction = "R" then
-        is_empty field (c ^+ (1, 0))
-      else false
-  | SysCandidates _ | SysRobot -> failwith "is_walkable got some internal system elements"
+  | Rock  -> true (* sic *)
+  | SysRobot -> failwith "is_walkable got some SysRobot, that should not happen"
 
 
 let update_robot coords = function
@@ -266,131 +267,101 @@ let exec_action field ?(allow_unwalkable=false) action =
 
   if action = "A" then raise (Finished field.score);
 
-  let old_map = field.f in
-
   let new_robo_coords = update_robot field.robot action in
-  (* nevar paiet, stāvi uz vietas *)
-
-
-  (* XXX needs to look at the new map and old map as well *)
-  let new_robo_coords =
-  if not & is_walkable field action new_robo_coords then (
-    if allow_unwalkable then field.robot else raise Bork_unwalkable;
-  ) else new_robo_coords in
-
-  let peek = peek_map ~robot:new_robo_coords in
 
   let score_diff, lambda_diff =
-  match peek_map old_map new_robo_coords with
+  match peek_map field.f new_robo_coords with
   | Earth -> -1, 0
   | Lambda -> 49, 1
   | _ -> -1, 0
   in
 
+  (* do robot action *)
+  let do_robo_stuff map = match (peek_map map new_robo_coords) with
+  | Wall -> raise Bork_unwalkable
+  | Beard _ -> raise Bork_unwalkable
+  | Empty -> map
+  | Razor -> CoordMap.remove new_robo_coords map
+  | Earth -> CoordMap.remove new_robo_coords map
+  | Lambda -> CoordMap.remove new_robo_coords map
+  | Lift -> if field.lambdas = 0 then raise (Finished (field.score + 25 * field.lambdas_eaten - 1)) else raise Bork_unwalkable
+  | Rock ->
+        let rock_moved_to = (if action = "L" then new_robo_coords ^+ (-1, 0) else if action = "R" then new_robo_coords ^+ (1, 0) else raise Bork_unwalkable) in
+        if not & CoordMap.mem rock_moved_to map then (
+          CoordMap.add rock_moved_to Rock (CoordMap.remove new_robo_coords map)
+        ) else (
+          raise Bork_unwalkable
+        )
+  | SysRobot -> failwith "SysRobot in do_robo_stuff"
 
-  let rec update_location coords loc =
+  in
 
-    if loc = Wall then coords, loc else
-    if coords = new_robo_coords then (
+  let map_after_robot_movement = do_robo_stuff field.f in
+  let peek = peek_map ~robot:new_robo_coords map_after_robot_movement in
 
-      (* razor taken, handled at the new_razors calculation *)
-      if loc = Razor then coords, Empty
+  let rec transform_location coords loc map =
+
+
+    match loc with
+    | Rock -> begin
+      let c_bottom = coords ^+ (+0, -1)
+      and c_left   = coords ^+ (-1, +0)
+      and c_right  = coords ^+ (+1, +0)
+      and c_bottom_left  = coords ^+ (-1, -1)
+      and c_bottom_right = coords ^+ (+1, -1) in
+      if peek c_bottom = Empty then (
+        (* akmens krīt lejā vertikāli *)
+        if new_robo_coords = coords ^+ (+0, -2) then raise Bork_fallen_rock;
+        CoordMap.add c_bottom Rock (CoordMap.remove coords map)
+      ) else
+      if peek c_bottom = Rock && peek c_right = Empty && peek c_bottom_right = Empty then (
+          (* uz leju pa labi *)
+          if new_robo_coords = coords ^+ (+1, -2) then raise Bork_fallen_rock;
+          CoordMap.add c_bottom_right Rock (CoordMap.remove coords map)
+      ) else
+      if peek c_bottom = Lambda && peek c_right = Empty && peek c_bottom_right = Empty then (
+          (* uz leju pa labi *)
+          if new_robo_coords = coords ^+ (+1, -2) then raise Bork_fallen_rock;
+          CoordMap.add c_bottom_right Rock (CoordMap.remove coords map)
+      ) else
+      if peek c_bottom = Rock && (peek c_left = Empty && peek c_bottom_left = Empty) && (peek c_right <> Empty || peek c_bottom_right <> Empty) then (
+        if new_robo_coords = coords ^+ (-1, -2) then raise Bork_fallen_rock;
+        CoordMap.add c_bottom_left Rock (CoordMap.remove coords map)
+      ) else
+      map
+    end
+    | Beard n -> begin
+      let m = CoordMap.remove coords map in
+      if n = field.growth - 1 then
+        let m = let c = (-1, -1) ^+ coords in if not & CoordMap.mem c map then CoordMap.add c (Beard 0) m else m in
+        let m = let c = (+0, -1) ^+ coords in if not & CoordMap.mem c map then CoordMap.add c (Beard 0) m else m in
+        let m = let c = (+1, -1) ^+ coords in if not & CoordMap.mem c map then CoordMap.add c (Beard 0) m else m in
+        let m = let c = (-1, +0) ^+ coords in if not & CoordMap.mem c map then CoordMap.add c (Beard 0) m else m in
+
+        let m = let c = (+1, +0) ^+ coords in if not & CoordMap.mem c map then CoordMap.add c (Beard 0) m else m in
+        let m = let c = (-1, +1) ^+ coords in if not & CoordMap.mem c map then CoordMap.add c (Beard 0) m else m in
+        let m = let c = (+0, +1) ^+ coords in if not & CoordMap.mem c map then CoordMap.add c (Beard 0) m else m in
+        let m = let c = (+1, +1) ^+ coords in if not & CoordMap.mem c map then CoordMap.add c (Beard 0) m else m in
+        CoordMap.add coords (Beard 0) m
       else
-
-      (* robots uzkāpj uz zemes, zeme pazūd: skipojam cellu *)
-      if loc = Earth then coords, Empty
-      else
-
-      (* robots apēd lambdu *)
-      if loc = Lambda then coords, Empty
-      else
-
-      (* robots iekāpj liftā *)
-      if loc = Lift then
-        (* XXX raise (Finished (field.score + 50 * field.lambdas_eaten - 1)) *)
-        raise (Finished (field.score + 25 * field.lambdas_eaten - 1))
-      else
-
-      (* robots grūž akmeni *)
-      if loc = Rock then
-        let delta = (if action = "L" then (-1, 0) else (1, 0)) in
-        update_location (coords ^+ delta) loc
-      else (coords, loc)
-
-    ) else (
-
-      match loc with
-      | Rock -> begin
-        let c_bottom = coords ^+ (+0, -1)
-        and c_left   = coords ^+ (-1, +0)
-        and c_right  = coords ^+ (+1, +0)
-        and c_bottom_left  = coords ^+ (-1, -1)
-        and c_bottom_right = coords ^+ (+1, -1) in
-        if peek old_map c_bottom = Empty && c_bottom <> new_robo_coords then (
-          (* akmens krīt lejā vertikāli *)
-          if new_robo_coords = coords ^+ (+0, -2) then raise Bork_fallen_rock;
-          (c_bottom, loc)
-        ) else
-        if peek old_map c_bottom = Rock && peek old_map c_right = Empty && peek old_map c_bottom_right = Empty then (
-            (* uz leju pa labi *)
-            if new_robo_coords = coords ^+ (+1, -2) then raise Bork_fallen_rock;
-            (c_bottom_right, loc)
-        ) else
-        if peek old_map c_bottom = Lambda && peek old_map c_right = Empty && peek old_map c_bottom_right = Empty then (
-            (* uz leju pa labi *)
-            if new_robo_coords = coords ^+ (+1, -2) then raise Bork_fallen_rock;
-            (c_bottom_right, loc)
-        ) else
-        if peek old_map c_bottom = Rock && (peek old_map c_left = Empty && peek old_map c_bottom_left = Empty) && (peek old_map c_right <> Empty || peek old_map c_bottom_right <> Empty) then (
-          if new_robo_coords = coords ^+ (-1, -2) then raise Bork_fallen_rock;
-          (c_bottom_left, loc)
-        ) else
-          (coords, loc)
-      end
-      | Beard n -> begin
-        if n = field.growth - 1 then
-          coords, SysCandidates
-          [ (coords, Beard 0)
-          ; (let c = (-1, -1) ^+ coords in c, Beard 0)
-          ; (let c = (-1, +0) ^+ coords in c, Beard 0)
-          ; (let c = (-1, +1) ^+ coords in c, Beard 0)
-          ; (let c = (+0, -1) ^+ coords in c, Beard 0)
-
-          ; (let c = (+0, +1) ^+ coords in c, Beard 0)
-          ; (let c = (+1, -1) ^+ coords in c, Beard 0)
-          ; (let c = (+1, +0) ^+ coords in c, Beard 0)
-          ; (let c = (+1, +1) ^+ coords in c, Beard 0)
-          ]
-        else (coords, Beard (n + 1));
-      end
-      | _ -> coords, loc
-    )
+        CoordMap.add coords (Beard (n + 1)) m
+    end
+    | _ -> map
   in
 
   let new_razors = field.razors
-    + if peek old_map new_robo_coords = Razor then 1 else 0
+    + if peek_map field.f new_robo_coords = Razor then 1 else 0
     + if action = "S" then -1 else 0
   in
 
-  let nf = CoordMap.fold
-    (fun coords elem new_map -> match update_location coords elem with
-    | new_coords, Empty -> new_map
-    | new_coords, SysCandidates cs ->
-        (* first candidate is definite, its existing beard *)
-        let (b_coords, b_elem) = List.hd cs in
-        CoordMap.add b_coords b_elem (List.fold_left (fun map (c,e) -> if c = new_robo_coords || CoordMap.mem c map then map else CoordMap.add c e map) new_map (List.tl cs))
-    | new_coords, elem -> CoordMap.add new_coords elem new_map)
-    field.f
-    CoordMap.empty in
 
-  let cur_water = if field.cur_water + 1 > field.flooding then 0 else field.cur_water + 1 in
+  let nf = CoordMap.fold transform_location map_after_robot_movement map_after_robot_movement in
+
+  let cur_water = if field.cur_water + 1 >= field.flooding then 0 else field.cur_water + 1 in
   let water = if cur_water = 0 && field.flooding > 0 then field.water + 1 else field.water in
   let robo_y = snd new_robo_coords in
   let cur_wetness = if robo_y <= water then field.cur_wetness + 1 else 0 in
-  if cur_wetness > field.waterproof then (
-    raise Bork_drowned;
-  );
-  (* if cur_wetness > 0 then log "Achievement achieved: got wet %d" cur_wetness; *)
+  if cur_wetness > field.waterproof then raise Bork_drowned;
 
   { field with
     f = nf;
@@ -552,7 +523,7 @@ let torture_chambers =
   ; "/home/w/projekti/icfp12/maps/contest7.map", 869
   ; "/home/w/projekti/icfp12/maps/contest8.map", 1973
   ; "/home/w/projekti/icfp12/maps/contest9.map", 3093
-  (* ; "/home/w/projekti/icfp12/maps/contest10.map", 3634 *)
+  ; "/home/w/projekti/icfp12/maps/contest10.map", 3634
   ; "/home/w/projekti/icfp12/maps/flood1.map", 945
   ; "/home/w/projekti/icfp12/maps/flood2.map", 281
   ; "/home/w/projekti/icfp12/maps/flood3.map", 1303
@@ -616,6 +587,9 @@ in
 
   let n = proper_solution_from_move_list ["A"; "B"; "C"] in
   assert (n = "CBA");
+
+  let n = rstrip " ABC " in
+  assert (n = " ABC");
 
   log "Tests passed"
 
